833cc7bc520986aa96508efe487c6287934c9bdf
Ronaldo Silveira <contato@expertme.com.br>
2024-12-27 14:59:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Cliente from './components/cliente/cliente';
-
-const clientes = [
-    {nome:"Ronaldo", cidade:"Mar de Espanha"},
-    {nome:"Mazu", cidade:"Juiz de Fora"},
-    {nome:"Raquel", cidade:"Santo Antonio do Aventureiro"},
-    {nome:"Vania", cidade:"Mar de Espanha"}
-  ];
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
